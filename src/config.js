dbb99a6a8b0a8e2734dfd195999de250205b0751
mitchellgsides <mitchellgsides@gmail.com>
2019-04-25 09:58:14 -0500

    config options

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
   API_KEY: process.env.API_KEY,
-  API_ENDPOINT: process.env.API_ENDPOINT,
-  Thing: "https://salty-sea-75049.herokuapp.com"
+  API_ENDPOINT: process.env.API_ENDPOINT
 };
